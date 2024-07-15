88d99b160436b435f4ef182220b1b6de7945b008
Ben Rehmann <brehmann@me.com>
2024-07-15 10:50:05 -0500

    first

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <h1>Open</h1>
+      </div>
+      <div>
+        <h1>Your</h1>
+      </div>
+      <div>
+        <h1>Mind</h1>
+      </div>
+      
     </div>
   );
 }
